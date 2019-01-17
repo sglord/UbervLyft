b84250106332719adee2bad26b9bfcb18bcedf19
Stephen Lord <stephenlord@Stephens-MacBook-Pro-2.local>
2019-01-17 17:33:14 -0500

    tested and confirmed uber API routes and data acquisition: products, price, time

--- ToDo.md
+++ ToDo.md
@@ -12,6 +12,10 @@ render login page for uber
 render login page for lyft
 render login page for google
 load app utilizing those user connections
+//without login
+//request request-promise
+//key
+//server to store
 
 As a user I want to call a car
 starting point input
@@ -21,8 +25,8 @@ option to change starting point
 utilize google map locations
 grab LatLong values
 destination input
-allow google map popular locations
 searchable menu/destination bar
+allow google map popular locations
 allow user google map saved locations
 return gps values when selected
 request car action
@@ -48,12 +52,53 @@ notification permissions
 when car has arrived push notification to user
 notification permissions
 
-Phase 2
-display map with current location pin and destination pin
-Phase 3
+PHASE 2
+Display map with current location pin and destination pin
+
+PHASE 3
+Display route after car arrives
+
+PHASE 4
+display uber and lyft vehicles on map
+display requested car on map
+display
+display running total saved
 
 other
 store user home/work/special
 show uber/lyft UI items
 ride history, both together
 show location to friends/contacts
+
+As a user I want to download the app*
+need to deploy to App Store*
+As a user I want to open the app
+create splash screen
+create app icon
+As a user I want to link my accounts
+open app
+render login page for uber
+
+    render login page for lyft
+    render login page for google
+    load app utilizing those user connections
+
+As a user I want to call a car
+render call car button
+Redn
+grab user current location
+permissions for location
+option to change starting point
+utilize google map locations
+grab LatLong values
+offer input for destination
+load google map popular locations
+searchable menu/destination bar
+load user google map saved locations
+return gps values when selected
+
+    make request to uber
+    display uber option
+    display lyft option
+
+As a user I want to be notified when my car is here
---
+++ UberTEST.js
@@ -0,0 +1,147 @@
+var Uber = require('node-uber');
+var express = require('express');
+var app = express();
+const axios = require('axios');
+
+var options = {
+	sandbox: true,
+	client_id: 'U1SYI1yL12LbnDkZd5T3KVJXLrLKNzMO',
+	client_secret: 'ZYsY5VD_Ej3XeJpDh6_We09SouLGxNtw3MVpnyt',
+	server_token: 'xXuaiNgnK2lmO2YTN2jTsJs61R8ZGXA05m9_rC4f',
+	redirect_uri: 'http://localhost:3000/callback',
+	language: 'en_US',
+	sandbox: true
+};
+
+var uber = new Uber(options);
+let config = {
+	headers: {
+		Authorization: 'Token xXuaiNgnK2lmO2YTN2jTsJs61R8ZGXA05m9_rC4f',
+		'Accept-Language': 'en_US',
+		'Content-Type': 'application/json'
+	}
+};
+
+app.get('/', function(req, res) {
+	// Kick off the authentication process
+	var scope = ['request'];
+	res.redirect(
+		uber.getAuthorizeUrl(scope, 'http://localhost:3000/auth/uber/callback')
+	);
+});
+
+app.get('/auth/uber/callback', function(req, res) {
+	uber.authorization(
+		{ grantType: 'authorization_code', authorization_code: req.query.code },
+		function(err, access_token) {
+			console.log(req.query.code);
+			// Now we've got an access token we can use to book rides.
+			// Access tokens expires in 30 days at whichpoint you can refresh.
+			// You should save this token
+			// More info: https://developer.uber.com/docs/authentication
+			uber.access_token = access_token;
+			res.send('Got token! /book to initiate an ride request.');
+		}
+	);
+});
+
+app.get('/book', function(req, res) {
+	var rideDetails = {
+		start_latitude: 37.78825,
+		start_longitude: -122.4324,
+		product_id: 'a1111c8c-c720-46c3-8534-2fcdd730040d' // SF Uber X
+	};
+
+	uber.requests.requestRide(rideDetails, function(err, result) {
+		if (err) {
+			// Failed
+			console.log(err);
+		} else {
+			console.log(result, 'RESULT');
+			res.json(result);
+		}
+	});
+});
+
+app.get('/products', async (request, res, next) => {
+	try {
+		const { data } = await axios.request(
+			'https://api.uber.com/v1.2/products?latitude=37.7752315&longitude=-122.418075',
+			config
+		);
+		console.log(data);
+		res.json(data);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+app.get('/products/:id', async (req, res, next) => {
+	let id = req.params.id;
+	try {
+		const { data } = await axios.request(
+			`https://api.uber.com/v1.2/products/${id}`,
+			config
+		);
+		console.log(data);
+		res.json(data);
+	} catch (error) {
+		console.log(error);
+	}
+});
+app.get('/estimate/price', async (req, res, next) => {
+	try {
+		const { data } = await axios.request(
+			`https://api.uber.com/v1.2/estimates/price?start_latitude=37.7752315&start_longitude=-122.418075&end_latitude=37.7752415&end_longitude=-122.518075`,
+			config
+		);
+		const comparePrice = (a, b) => {
+			if (a.high_estimate === null) {
+				return 1;
+			}
+			if (b.high_estimate === null) {
+				return -1;
+			}
+			return (
+				(a.high_estimate + a.low_estimate) / 2 -
+				(b.high_estimate + b.low_estimate) / 2
+			);
+		};
+		data.prices.sort(comparePrice);
+		//VVVVV return lowest single itemVVVVV
+		// res.json(data.prices[0]);
+		//VVVV return sorted data VVVVVV
+		res.json(data);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+app.get('/estimate/time', async (req, res, next) => {
+	try {
+		const { data } = await axios.request(
+			'https://api.uber.com/v1.2/estimates/time?start_latitude=37.7752315&start_longitude=-122.418075',
+			config
+		);
+		const compareTime = (a, b) => {
+			return a.estimate - b.estimate;
+		};
+		data.times.sort(compareTime);
+		//VVVVV return lowest single itemVVVVV
+		// res.json(data.times[0]);
+		//VVVV return sorted data VVVVVV
+		res.json(data);
+	} catch (error) {
+		console.log(error);
+	}
+});
+
+app.listen(3000, function() {
+	console.log('Listening on port 3000!');
+});
+
+// start lat
+// start long
+// end lat
+// end long
+// config*
